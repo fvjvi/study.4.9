3e6ba7c98443ed882b3ad3bcd7506f7991d1a6f9
fvjvi <121670974+fvjvi@users.noreply.github.com>
2024-04-15 19:42:21 +0800

    Update index.js

--- study4.9/js/index.js
+++ study4.9/js/index.js
@@ -1,28 +1,50 @@
-var toLogin = document.getElementById('toLogin');
-var toRegister = document.getElementById('toRegister');
-var form_box = document.getElementsByClassName('form-box')[0];
-var register_box = document.getElementsByClassName('register-box')[0];
-var login_box = document.getElementsByClassName('login-box')[0];
-toRegister.addEventListener('click',()=>{
-    form_box.style.transform='translateX(100.5%)';
-    login_box.classList.add('hidden');
-    register_box.classList.remove('hidden');
-    document.getElementById('logUN').value = "";
-    document.getElementById('logPW').value = "";
-    document.getElementById('regUN').value = "";
-    document.getElementById('regPW').value = "";
-})
-toLogin.addEventListener('click',()=>{
-    form_box.style.transform='translateX(0%)';
-    register_box.classList.add('hidden');
-    login_box.classList.remove('hidden');
-    document.getElementById('logUN').value = "";
-    document.getElementById('logPW').value = "";
-    document.getElementById('regUN').value = "";
-    document.getElementById('regPW').value = "";
-})
-
-
+if(window.innerWidth > 500) {
+    var toLogin = document.getElementById('toLogin');
+    var toRegister = document.getElementById('toRegister');
+    var form_box = document.getElementsByClassName('form-box')[0];
+    var register_box = document.getElementsByClassName('register-box')[0];
+    var login_box = document.getElementsByClassName('login-box')[0];
+    toRegister.addEventListener('click',()=>{
+        form_box.style.transform='translateX(100.5%)';
+        login_box.classList.add('hidden');
+        register_box.classList.remove('hidden');
+        document.getElementById('logUN').value = "";
+        document.getElementById('logPW').value = "";
+        document.getElementById('regUN').value = "";
+        document.getElementById('regPW').value = "";
+    })
+    toLogin.addEventListener('click',()=>{
+        form_box.style.transform='translateX(0%)';
+        register_box.classList.add('hidden');
+        login_box.classList.remove('hidden');
+        document.getElementById('logUN').value = "";
+        document.getElementById('logPW').value = "";
+        document.getElementById('regUN').value = "";
+        document.getElementById('regPW').value = "";
+    })
+}else{
+    var toLogin = document.getElementById('toLoginp');
+    var toRegister = document.getElementById('toRegisterp');
+    var form_box = document.getElementsByClassName('form-box')[0];
+    var register_box = document.getElementsByClassName('register-box')[0];
+    var login_box = document.getElementsByClassName('login-box')[0];
+    toRegister.addEventListener('click',()=>{
+        login_box.classList.add('hidden');
+        register_box.classList.remove('hidden');
+        document.getElementById('logUN').value = "";
+        document.getElementById('logPW').value = "";
+        document.getElementById('regUN').value = "";
+        document.getElementById('regPW').value = "";
+    })
+    toLogin.addEventListener('click',()=>{
+        register_box.classList.add('hidden');
+        login_box.classList.remove('hidden');
+        document.getElementById('logUN').value = "";
+        document.getElementById('logPW').value = "";
+        document.getElementById('regUN').value = "";
+        document.getElementById('regPW').value = "";
+    })
+}
 function log(){
     let logUN = document.getElementById('logUN').value;
     let logPW = document.getElementById('logPW').value;
